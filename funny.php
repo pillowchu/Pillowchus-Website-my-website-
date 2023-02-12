3721ccde307ca9a983e1c9d9b57b657508d3a3c9
Envyslife <envxssss@gmail.com>
2023-02-12 16:05:51 +0000

    Update funny.php

--- funny.php
+++ funny.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "My first PHP script!";
+echo "Kaidens a gay bastard who sucks johns toes for free, he has commited to mnay war crimes and is planning on bombing ukraine with his farts and toots. This man needs to be arressted for his own good";
 ?>
 
 </body>
